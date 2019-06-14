b34324a1085a38dd4870b6634586793bd754ffd8
Denis Bystruev <dbystruev@me.com>
2019-06-14 07:23:04 +0300

    Changed configuration and routes for User

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -11,10 +11,31 @@ public func routes(_ router: Router) throws {
     router.get("hello") { req in
         return "Hello, world!"
     }
+    
+    router.get("hello", String.parameter) { req -> String in
+        let name = try req.parameters.next(String.self)
+        return "Hello, \(name)!"
+    }
+    
+    router.post("info") { req -> InfoResponse in
+        let data = try req.content.syncDecode(InfoData.self)
+        return InfoResponse(request: data)
+    }
+    
+//    // Example of configuring a controller
+//    let todoController = TodoController()
+//    router.get("todos", use: todoController.index)
+//    router.post("todos", use: todoController.create)
+//    router.delete("todos", Todo.parameter, use: todoController.delete)
+    
+    let userController = UserController()
+    try router.register(collection: userController)
+}
 
-    // Example of configuring a controller
-    let todoController = TodoController()
-    router.get("todos", use: todoController.index)
-    router.post("todos", use: todoController.create)
-    router.delete("todos", Todo.parameter, use: todoController.delete)
+struct InfoData: Content {
+    let name: String
+}
+
+struct InfoResponse: Content {
+    let request: InfoData
 }
